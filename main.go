5c2e6255a5cf0eff617774a9625d072cfbd8a758
Moses Ichado <ichadomoses@gmail.com>
2021-08-31 17:27:29 -0700

    CRUD API commit

--- main.go
+++ main.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	"os"
-
 	"github.com/gin-gonic/gin"
 )
 
+
+
+// multipleUsers := [User{Micheal, James, Timmy}]
+// Micheal
+
+type User struct {
+	Name string `json:"name"`
+	Age int `json:"age"`
+	Email string `json:"email"`
+	Country string `json:"country"`
+}
+
 func main() {
 	router := gin.Default()
-	router.GET("/", helloWorldhandler)
-	router.POST("/", goodhandler)
+	router.GET("/getUsers", getHandler)
+	router.POST("/createUsers", createHandler)
+	router.PATCH("/updateUsers", updateHandler)
+	router.DELETE("/delete", deleteHandler)
+	
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -20,13 +34,44 @@ func main() {
 
 }
 
-func helloWorldhandler(c *gin.Context) {
+func getHandler(c *gin.Context) {
+	var user User
+	user = User{
+		Name: "John Doe",
+		Age: 39,
+		Email: "johndoe@gmail.com",
+		Country: "Cameroon",
+	}
 	c.JSON(200, gin.H{
 		"message": "Hello Guys",
+		"data": user,
 	})
 }
 
-func goodhandler(c *gin.Context) {
+func createHandler(c *gin.Context) {
+	var user User
+
+	err := c.ShouldBindJSON(&user)
+	if err != nil {
+	c.JSON(400, gin.H{
+			"error": "invalid request data",
+		})
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"message": "Profile successfully created",
+		"data": user,
+	})
+}
+
+func updateHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Profile Updated",
+	})
+}
+
+func deleteHandler(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "Good People",
 	})
